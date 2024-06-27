872f331b7c8d67bcee7b7f4f17535c29db06c8be
Axel Montes Salazar <38364189+Darkar131@users.noreply.github.com>
2024-06-27 11:14:09 -0700

    Update Sorting.js

--- assets/js/Sorting.js
+++ assets/js/Sorting.js
@@ -1,21 +1,25 @@
 //Sorting Algorithm, use for Alphabetically sort courses from A to Z
-function sortList() {
-  var list, i, switching, b, shouldSwitch;
-  list = document.getElementById("sortby");
-  switching = true;
-  while (switching) {
-    switching = false;
-    b = list.getElementsByTagName("LI");
-    for (i = 0; i < (b.length - 1); i++) {
-      shouldSwitch = false;
-      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
-      switching = true;
-    }
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+            sortList();
+        });
+
+        function sortList() {
+            var list, i, switching, b, shouldSwitch;
+            list = document.getElementById("sortingby");
+            switching = true;
+            while (switching) {
+                switching = false;
+                b = list.getElementsByTagName("LI");
+                for (i = 0; i < (b.length - 1); i++) {
+                    shouldSwitch = false;
+                    if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+                if (shouldSwitch) {
+                    b[i].parentNode.insertBefore(b[i + 1], b[i]);
+                    switching = true;
+                }
+            }
+        }
